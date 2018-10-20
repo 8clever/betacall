f5fc0145d4e7863a42c85e329ed71f0459aefbd4
ivanv <ivanv@pushok.com>
2018-10-20 18:24:56 +0300

    add small fix

--- modules/asterisk/index.js
+++ modules/asterisk/index.js
@@ -55,12 +55,6 @@ api._startCalls = async function(t, p) {
             }
         });
 
-        console.log(orderId)
-        console.log(dontCall.list)
-
-        console.log(orders[1])
-        return;
-
         if (!dontCall.count) {
             let phone = order.clientInfo.phone;
             await this._call(t, { phone });
