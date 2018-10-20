403bddfd8c00e1341849e850e65f2e56fd8011d8
ivanv <ivanv@pushok.com>
2018-10-20 19:36:50 +0300

    add small fix

--- modules/asterisk/index.js
+++ modules/asterisk/index.js
@@ -97,8 +97,6 @@ api._call = async function(t, {
     let serverIo = await ctx.api.socket.getServerIo(t, {});
 
     ami.on(id, async evt => {
-        console.log(evt);
-
         if (evt.Event === "Hangup") {
             
             // not connecting
