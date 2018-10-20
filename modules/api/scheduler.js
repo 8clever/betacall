c864b46b6ba503721b168fcdb520d4072af1cbef
ivanv <ivanv@pushok.com>
2018-10-20 18:55:50 +0300

    add small fix

--- modules/api/scheduler.js
+++ modules/api/scheduler.js
@@ -16,6 +16,7 @@ module.exports.init = async function (_ctx) {
 	api.addJob("order._getCallOrders", { cronTime: "0 */15 * * * *" });
 	api.addJob("asterisk._startCalls", { cronTime: "*/10 * * * * *" });
 	api.addJob("asterisk._processUnnavailableCalls", { cronTime: "0 0 */1 * * *" });
+	api.addJob("asterisk._phoneLogs", { cronTime: "*/10 * * * * *" });
 
 	return { api };
 };
