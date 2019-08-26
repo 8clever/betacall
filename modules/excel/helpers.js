9723dafbdedaa24c4c8fb4f2fd66bcd77bfd03de
ivanv <ivanv@gost-group.com>
2019-08-26 09:30:22 -0700

    add fix

--- modules/excel/helpers.js
+++ modules/excel/helpers.js
@@ -97,10 +97,10 @@ function getQuery (filter, ctx) {
   query.$and = query.$and || [];
 
   _.each(ctx.cfg.ami.blackList, black => {
-    const re = new RegExp(black);
+    const re = new RegExp(`^(?!${black}).+$`);
     query.$and.push({
         _s_phone: {
-            $not: re, 
+            $regex: re, 
         }
     })
   });
