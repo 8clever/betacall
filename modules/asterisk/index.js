a4c04485846e18d9b8826746946744ec704b6c5e
Витяев Иван Павлович <ivan.vitiaev@gost-group.com>
2021-04-18 11:29:25 +0300

    add small fix

--- modules/asterisk/index.js
+++ modules/asterisk/index.js
@@ -87,7 +87,8 @@ module.exports.init = async function (...args) {
 
             const holdTimeListener = _.debounce(async (evt) => {
                 const io = await ctx.api.socket.getIo();
-                let user = await ctx.api.users.getUsers(t, { login: evt.Exten });
+                const t = await ctx.api.scheduler._getRobotToken("", {});
+                const user = await ctx.api.users.getUsers(t, { login: evt.DestCallerIDNum });
                 if (user) {
                     io.emit(`${user._id}_holdTime`, evt.HoldTime);
                 }
